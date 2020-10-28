824b9d85e15a2a390d77ac31392fbd551bf40a34
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-10-29 01:16:55 +0530

    Initial Commit

---
+++ WatchApp/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp
+//
+//  Created by Mahesh Prasad on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
